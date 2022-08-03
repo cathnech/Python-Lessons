76e52dc8b26e6b62ad40d729633fddcb2878edd6
cathnech <cathnech@gmail.com>
2022-08-04 02:07:02 +0300

    hw3,fix

--- HW_3/hw3.py
+++ HW_3/hw3.py
@@ -84,7 +84,10 @@ def funk_febonachi(n):
     ans_arr = [0]
     j = 1
     for i in range(1,n+1):
-        ans_arr.insert(0,-j)
+        if i%2 == 0:
+            ans_arr.insert(0,-j)
+        else:
+            ans_arr.insert(0,j)
         ans_arr.append(j)
         j += ans_arr[-2]
     return ans_arr
